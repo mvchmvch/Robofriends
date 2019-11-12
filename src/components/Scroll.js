e0674f54b70b96b0e0390fa9b7799835895a0ac0
Mikhail Chudakov <mvch@list.ru>
2019-11-12 19:03:54 +0300

    adjusted the scroll component height

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ const Scroll = (props) => {
 	return (
 		<div style={{overflowY:'scroll', 
 					border: '5px solid black', 
-					height: '1000px'}}>
+					height: '800px'}}>
 			{props.children}
 		</div>
 	)
