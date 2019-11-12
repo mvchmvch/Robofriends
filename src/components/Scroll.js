f52028df746b8d513ac794b349c4f805cae3756e
Mikhail Chudakov <mvch@list.ru>
2019-11-12 19:01:04 +0300

    changed setup to be publeshed on gh-pages

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ const Scroll = (props) => {
 	return (
 		<div style={{overflowY:'scroll', 
 					border: '5px solid black', 
-					height: '600px'}}>
+					height: '1000px'}}>
 			{props.children}
 		</div>
 	)
