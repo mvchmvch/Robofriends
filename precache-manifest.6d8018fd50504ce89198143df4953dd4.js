bc2e1d8568f4b4be308efefd284f34726e7c545b
Mikhail Chudakov <mvch@list.ru>
2019-11-12 18:57:28 +0300

    Updates

---
+++ precache-manifest.6d8018fd50504ce89198143df4953dd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a4fa8d2f55f513abf79f29a5481ce7",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/Robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "0de48ca108d08aee8788",
+    "url": "/Robofriends/static/css/main.187d6237.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/Robofriends/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "0de48ca108d08aee8788",
+    "url": "/Robofriends/static/js/main.3a668e7d.chunk.js"
+  },
+  {
+    "revision": "52b272b411e279fbe9a6",
+    "url": "/Robofriends/static/js/runtime~main.14aa3d65.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
