1c7dc747722a52134876e21513cc209fea70015b
Mikhail Chudakov <mvch@list.ru>
2020-01-17 16:46:09 +0300

    added serviceWorker

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,8 +5,6 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
 import './App.css'
 
-import * as serviceWorker from '../serviceWorker';
-
 import {setSearchField} from '../actions';
 
 const mapStateToProps = state => {
@@ -56,9 +54,4 @@ class App extends Component {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
 
